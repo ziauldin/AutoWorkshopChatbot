e417310fd6012d630767169b0b672807773e929e
ziauldin <95315363+ziauldin@users.noreply.github.com>
2025-06-14 13:04:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -339,4 +339,4 @@ async def upload_image(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
